7a148e1b78773d44251e2c703f1e87cc42500849
SergiOn <Onishhenko@gmail.com>
2017-07-02 13:04:06 +0300

    #ng2-redux # @select

--- angular-ng2-redux/src/app/app.component.ts
+++ angular-ng2-redux/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NgRedux } from 'ng2-redux'
+import { NgRedux, select } from 'ng2-redux'
 
 import { IAppState } from './store';
 import { INCREMENT } from './actions';
@@ -11,7 +11,7 @@ import { INCREMENT } from './actions';
 })
 export class AppComponent {
   title = 'app';
-  counter = 0;
+  @select() counter;
 
   constructor(
     private ngRedux: NgRedux<IAppState>
@@ -19,8 +19,6 @@ export class AppComponent {
     ngRedux.subscribe(() => {
       const store = ngRedux.getState();
       console.log(store);
-
-      this.counter = store.counter;
     });
   }
 
