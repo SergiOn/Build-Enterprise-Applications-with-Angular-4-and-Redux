6bc1085edac579a040ae2222e8ee51e53b7b184f
SergiOn <Onishhenko@gmail.com>
2017-07-02 13:20:21 +0300

    #ng2-redux # the select decorator

--- angular-ng2-redux/src/app/store.ts
+++ angular-ng2-redux/src/app/store.ts
@@ -1,23 +1,16 @@
 import { INCREMENT } from './actions';
 
 export interface IAppState {
-  counter: number,
-  messaging?: {
-    newMessages: number
-  }
+  counter: number
 }
 
 export const INITIAL_STATE: IAppState = {
   counter: 0,
-  messaging: {
-    newMessages: 5
-  }
 };
 
 export function rootReducer(state: IAppState, action): IAppState {
   switch (action.type) {
-    case INCREMENT: return { ...state, counter: state.counter + 1 };
-    // case INCREMENT: return { counter: state.counter + 1 }; // select with function save value newMessages
+    case INCREMENT: return { counter: state.counter + 1 };
   }
 
   return state;
