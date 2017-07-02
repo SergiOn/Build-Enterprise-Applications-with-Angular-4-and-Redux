bbbdd8e6abe7dd5cb13594d0105ddda23b058c73
SergiOn <Onishhenko@gmail.com>
2017-07-02 12:49:52 +0300

    #ng2-redux # create architecture

--- angular-ng2-redux/src/app/app.component.ts
+++ angular-ng2-redux/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { NgRedux } from 'ng2-redux'
+
+import { IAppState } from './store';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  counter = 0;
+
+  constructor(
+    private ngRedux: NgRedux<IAppState>
+  ) {}
+
+  increment() {
+    this.ngRedux.dispatch({ type: 'INCREMENT' });
+  }
+
 }
--- angular-ng2-redux/src/app/app.module.ts
+++ angular-ng2-redux/src/app/app.module.ts
@@ -21,6 +21,6 @@ export class AppModule {
   constructor(
     ngRedux: NgRedux<IAppState>
   ) {
-    ngRedux.configureStore(rootReducer, {});
+    ngRedux.configureStore(rootReducer, { counter: 0 });
   }
 }
