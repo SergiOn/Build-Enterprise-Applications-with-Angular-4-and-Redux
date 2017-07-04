30e62b1f69b69d4ff468709ba14f77c587ddda47
SergiOn <onishhenko@gmail.com>
2017-07-04 16:24:29 +0300

    #angular-ng2-redux # begin

--- angular-ng2-redux/src/app/app.component.ts
+++ angular-ng2-redux/src/app/app.component.ts
@@ -1,9 +1,4 @@
 import { Component } from '@angular/core';
-import { NgRedux, select } from 'ng2-redux'
-import { Map } from 'immutable'
-
-import { IAppState } from './store';
-import { INCREMENT } from './actions';
 
 @Component({
   selector: 'app-root',
@@ -11,20 +6,5 @@ import { INCREMENT } from './actions';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
-  @select(s => s.get('counter')) count;
-
-  constructor(
-    private ngRedux: NgRedux<Map<string, any>>
-  ) {
-    ngRedux.subscribe(() => {
-      const store = ngRedux.getState();
-      console.log(store);
-    });
-  }
-
-  increment() {
-    this.ngRedux.dispatch({ type: INCREMENT });
-  }
-
+  title = 'app works!';
 }
